c34329e7f04ced5e545628e7fe0d0d9fb01b6a2b
jacobhrussell <jacobhenryrussell@gmail.com>
2025-12-10 22:52:35 -0600

    merge conflict

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ApplicationRecord
-  include Entropic, Seedeable, MultiTenant
+  include Account::Storage, Entropic, Seedeable, MultiTenant
 
   has_one :join_code
   has_many :users, dependent: :destroy
